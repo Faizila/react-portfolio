307b26b4d0ee1f2c20d6bdfec518920dfbe92fc5
Faizila Amlani <amlani.faizila@gmail.com>
2021-11-19 14:36:31 -0600

    setting up connection in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import Nav from './PortfolioContainer/Navbar/Navbar'
+import Profile from './PortfolioContainer/Home/Profile';
+import Aboutme from './PortfolioContainer/AboutMe/Aboutme';
+import Resume from './PortfolioContainer/Resume/Resume';
+import Portfolio from './PortfolioContainer/Portfolio/Portfolio';
+import Contact from './PortfolioContainer/ContactMe/Contact';
 
 function App() {
   return (
-    <div>
-
+    <div id="main">
+      <Nav/>
+      <Profile/>
+      <Aboutme/>
+      <Resume/>
+      <Portfolio/>
+      <Contact/>
     </div>
   );
 }
